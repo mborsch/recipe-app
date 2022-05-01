e0b73e6cd21c943d390a9dbf7b71c69352cd4a80
mborsch <matthewmborschnack@gmail.com>
2022-05-01 13:13:52 -0500

    Updates

---
+++ precache-manifest.9fb86b0586a10a2c78dcb36941caabfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f5219da01b1122e15797cde4b7a8180",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "1a53c4b6b42ecac38cf8",
+    "url": "/recipe-app/static/css/main.478cbe41.chunk.css"
+  },
+  {
+    "revision": "3911981887a75ec749fb",
+    "url": "/recipe-app/static/js/2.15440edb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/recipe-app/static/js/2.15440edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a53c4b6b42ecac38cf8",
+    "url": "/recipe-app/static/js/main.cb385f8d.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  }
+]);
